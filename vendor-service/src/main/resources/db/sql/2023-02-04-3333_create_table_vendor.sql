f07c2bfaa29c463e1149b577480194d7a065c3dc
Dmytro Kovalenko <Dmytro.Kovalenko@microfocus.com>
2023-02-06 12:18:23 +0200

    [+/*] Vendor service. Vendor type db enum added. Refactored.

--- vendor-service/src/main/resources/db/sql/2023-02-04-3333_create_table_vendor.sql
+++ vendor-service/src/main/resources/db/sql/2023-02-04-3333_create_table_vendor.sql
@@ -1,6 +1,6 @@
 CREATE TABLE public.vendor (
     vendor_       char(36) DEFAULT uuid_generate_v4()        NOT NULL,
-    type          varchar(100)                        UNIQUE NOT NULL,
+    type          vendor_type                         UNIQUE NOT NULL,
     display_name  varchar(255)                               NOT NULL,
     driver        varchar(100)                               NOT NULL,
     is_rdbms      boolean                                    NOT NULL,
